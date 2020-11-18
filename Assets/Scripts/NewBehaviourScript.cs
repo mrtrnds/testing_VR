cc48903671861783ae815747fe0d7bcafb4bda4d
mrtrnds <byeven@outlook.com>
2020-11-18 17:02:09 +0200

    2nd commit

--- Assets/Scripts/NewBehaviourScript.cs
+++ Assets/Scripts/NewBehaviourScript.cs
@@ -4,10 +4,22 @@ using UnityEngine;
 
 public class NewBehaviourScript : MonoBehaviour
 {
+    Color colorStart;
     // Start is called before the first frame update
     void Start()
     {
-        
+        colorStart = GetComponent<Renderer>().material.color;
+
+    }
+
+    private void OnMouseDown()
+    {
+        GetComponent<Renderer>().material.color = Color.red;
+    }
+
+    private void OnMouseUp()
+    {
+        GetComponent<Renderer>().material.color = colorStart;
     }
 
     // Update is called once per frame
